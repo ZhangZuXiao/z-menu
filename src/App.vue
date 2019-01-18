6a60d352746c7e666460d73df3b21aaf73d2d5b9
zzx <zhangzx22@lenovo.com>
2019-01-18 18:24:48 +0800

    --

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <span @click="showEl">我的第一个插件</span>
+    <z-menu  :data="data" ref="xxs" @zmClick="emclick"></z-menu>
+  </div>
+</template>
+
+<script>
+import zMenu from './lib/z-menu'
+
+export default {
+  name: 'app',
+  components:{
+    zMenu
+  },
+  data () {
+    return {
+      data: [{name:'张三'},{name:'李四'}]
+    }
+  },
+  methods: {
+    showEl(){
+      //this.$refs.xxs.zm_event();
+      //console.log( this.$refs.xxs);
+    },
+    emclick(name){
+        alert(name);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/lib/z-menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="z-menu">
+       <div class="z-menu-chunk" v-for="(item,index) in data">
+            <i @click="zm_event(item.name)">{{item.name}}</i>
+       </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'z-menu',
+    props: ['data','type'],
+    data () {
+      return {
+         msg:'msg ccmt'
+      }
+    },
+    methods: {
+      zm_event(name) {
+         this.$emit('zmClick',name);
+      },
+      
+    }
+  }
+</script>
+
+<style>
+  .z-menu{background: #000;width: 56px;height: 100%;float: left;}
+  .z-menu-chunk{width: 100%;height: 56px;background: red;}
+</style>
