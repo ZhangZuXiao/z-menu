a7e361207ca11f3756895d96709954ea520a6a78
zzx <zhangzx22@lenovo.com>
2019-01-21 11:32:33 +0800

    --

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,17 @@
 import zMenu from './z-menu.vue' // 导入组件
 
-const PayPop = {
+const ZzxMenu = {
     install(Vue, options) {
-        Vue.component(zMenu.name, zMenu) // vuePayPop.name 组件的name属性
+        Vue.component(zMenu.name, zMenu) // vueZzxMenu.name 组件的name属性
         // 类似通过 this.$xxx 方式调用插件的 其实只是挂载到原型上而已
         // Vue.prototype.$xxx  // 最终可以在任何地方通过 this.$xxx 调用
         // 虽然没有明确规定用$开头  但是大家都默认遵守这个规定
     }
 }
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(PayPop);
+  window.Vue.use(ZzxMenu);
 }
-export default PayPop
+export default ZzxMenu
 
 export {
     zMenu
